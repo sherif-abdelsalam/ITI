9e91bd367ab46ceb83da29b238a2696b55d3d9ce
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-02-05 01:48:38 +0200

    modified the main menue

--- Python/day_3/user_ops_menu.py
+++ Python/day_3/user_ops_menu.py
@@ -1,43 +1,49 @@
 
+from project_ops.delete_my_project import delete_my_project
+from project_ops.edit_user_project import edit_user_project
+from project_ops.search_by_date import search_by_date
+from project_ops.view_all_project import view_all_projects
 from project_ops.create_project import create_project
-from system_methods import clear_screen
+from system_methods import clear_screen, pause_for_1_seconds
 
 
 def main_menu(user_email: str):
 
-    clear_screen()
-
-    print('--------------------')
-    print("1. Create a project ")
-    print("2. View all projects")
-    print("3. Edit your own projects")
-    print("4. Delete your own projects")
-    print("5. Search for a project (date)")
-    print("6. Logout")
-    print('--------------------')
-
     while True:
+        clear_screen()
+
+        print('--------------------')
+        print("1. Create a project ")
+        print("2. View all projects")
+        print("3. Edit your own projects")
+        print("4. Delete your own projects")
+        print("5. Search for a project (date)")
+        print("6. Logout")
+        print('--------------------')
+
         choice = input("Enter your choice: ")
 
         if choice == '1':
             create_project(user_email)
 
         elif choice == '2':
-            pass
+            view_all_projects() 
 
         elif choice == '3':
-            pass
+            edit_user_project(user_email)
 
         elif choice == '4':
-            pass
+            delete_my_project(user_email)
 
         elif choice == '5':
-            pass
+            search_by_date()
 
         elif choice == '6':
             print("Logging out...")
+            pause_for_1_seconds()
             break
 
         else:
             print("Invalid Choice, Enter a number between 1-6")
+            pause_for_1_seconds()
 
