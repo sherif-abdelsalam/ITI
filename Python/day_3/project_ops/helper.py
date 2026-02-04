9abaceda88181fe80f2b17c94050328feb048a8f
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-02-05 01:44:27 +0200

    added helpter mthods for projects operaions

---
+++ Python/day_3/project_ops/helper.py
@@ -0,0 +1,30 @@
+
+from data_ops import load
+
+
+def get_my_projects(email: str):
+    projects = load("projects")
+    print()
+    user_projects = []
+    for project in projects:
+        if project["user"] == email:
+            user_projects.append(project)
+    return user_projects
+
+
+def select_user_project(projects, email):
+    if not projects:
+        return None
+
+    while True:
+        project_id = input("Enter the project ID: ")
+        if not project_id.isdigit():
+            print("Enter a valid project ID.")
+            continue
+        project_id = int(project_id)
+
+        for idx, project in enumerate(projects):
+            if project['id'] == project_id and project['user'] == email:
+                return idx
+
+        print("Project ID not found. Please try again.")
