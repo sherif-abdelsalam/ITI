4a04da6882d833f4ba60801184baa6e2dc65e752
sherif-abdelsalam <sherif.abdelsalam.111@gmail.com>
2026-02-26 15:07:50 +0200

    removed lab_3

--- Node/lab_3/Middleware/hashPassword.js
+++
@@ -1,9 +0,0 @@
-
-import bcrypt from "bcrypt"
-
-let hashPassword = (req,res,next) => {
-    req.body.password = bcrypt.hashSync(req.body.password, 10)
-    next();
-}
-
-export default hashPassword;
--- Node/lab_3/Utils/post_validaion.js
+++
@@ -1,9 +0,0 @@
-
-export const validatePost = (post) => {
-
-    if (!post.title || post.title.trim().length === 0 
-    || !post.content || post.content.trim().length === 0 ) {
-        return false
-    }
-    return true
-}
